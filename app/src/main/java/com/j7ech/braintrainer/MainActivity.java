1f9509b3e0c21a98ca024c05f8752ba1e0327c4a
Jagadeesh M <myqualitywriters1@gmail.com>
2020-07-31 15:34:13 +0530

    Initial Commit!

---
+++ app/src/main/java/com/j7ech/braintrainer/MainActivity.java
@@ -0,0 +1,22 @@
+package com.j7ech.braintrainer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button startButton;
+
+    public void startGame(View view) {
+        startButton.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startButton = findViewById(R.id.startButton);
+    }
+}
